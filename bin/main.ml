42a89b7655d6cd330af3dee7db644e7e0ff72ee6
Ken Sakayori <kensakayori+research@gmail.com>
2023-04-15 18:17:19 +0900

    add file with metadata to the archive
     Please enter the commit message for your changes. Lines starting

--- bin/main.ml
+++ bin/main.ml
@@ -90,11 +90,24 @@ let print_file_struct n =
   List.iter pr files;
   Printf.printf "└── %s\n" report
 
+let make_env_file archive =
+  (* This should go to the Util Module *)
+  let (let@) f k = f k in
+  let@ cout = Io.CPS.open_out (Printf.sprintf "%s/ENV" archive) in
+  let write s1 s2 = output_string cout @@ Printf.sprintf "%s: %s\n" s1 s2 in
+  write "ver" Config.version;
+  write "date" @@ MyUnix.open_and_read_line "date";
+  write "week" (Printf.sprintf "%02d" !Config.no);
+  write "ID" !Config.id;
+  write "ocaml" Config.ocaml_version;
+  write "system" @@ MyUnix.open_and_read_line "uname -a"
+
 let make_archive () =
   let archive = Printf.sprintf "%02d-%s" !Config.no !Config.id in
   let filename = Printf.sprintf "%02d-%s.zip" !Config.no !Config.id in
   Sys.chdir Config.orig_working;
   Sys.chdir Config.dir;
+  make_env_file archive;
   let r = Command.run ~filename:"zip" "zip -r %s %s" filename archive in
   if 0 <> r then
     (Command.mv ["zip.err"] Config.orig_working;
