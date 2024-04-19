dece489d3155296db090a3b9d98431854bc9937b
Akihisa Yagi <caphosra@gmail.com>
2024-04-19 14:32:20 +0900

    Add true_t to correct the types

--- bin/week02.ml
+++ bin/week02.ml
@@ -21,17 +21,17 @@ let assignments =
    hatten 2, [TypeOpt("prop1", "('a -> 'b) -> ('b -> 'c) -> 'a -> 'c");
               TypeOpt("prop2", "('a, ('b, 'c) and_t) or_t -> (('a, 'b) or_t, ('a, 'c) or_t) and_t");
               TypeOpt("prop3", "(('a, 'b) or_t, ('a, 'c) or_t) and_t -> ('a, ('b, 'c) and_t) or_t");
-              TypeOpt("prop4", "('a, 'a not_t) or_t");
-              TypeOpt("prop4", "('a -> 'c) -> ('a not_t -> 'c) -> 'c");
-              TypeOpt("prop5", "('a, 'a not_t) and_t");
-              TypeOpt("prop5", "('a -> 'a not_t -> 'c) -> 'c");
+              TypeOpt("prop4", "true_t -> ('a, 'a not_t) or_t");
+              TypeOpt("prop4", "true_t -> ('a -> 'c) -> ('a not_t -> 'c) -> 'c");
+              TypeOpt("prop5", "true_t -> ('a, 'a not_t) and_t");
+              TypeOpt("prop5", "true_t -> ('a -> 'a not_t -> 'c) -> 'c");
               TypeOpt("prop6", "(('a -> 'b) -> 'a) -> 'a");
               TypeOpt("prop_cc1", "('a -> 'b) -> ('b -> 'c) -> 'a -> 'c");
               TypeOpt("prop_cc2", "('a, ('b, 'c) and_t) or_t -> (('a, 'b) or_t, ('a, 'c) or_t) and_t");
               TypeOpt("prop_cc3", "(('a, 'b) or_t, ('a, 'c) or_t) and_t -> ('a, ('b, 'c) and_t) or_t");
-              TypeOpt("prop_cc4", "('a, 'a not_t) or_t");
-              TypeOpt("prop_cc4", "('a -> 'c) -> ('a not_t -> 'c) -> 'c");
-              TypeOpt("prop_cc5", "('a, 'a not_t) and_t");
-              TypeOpt("prop_cc5", "('a -> 'a not_t -> 'c) -> 'c");
+              TypeOpt("prop_cc4", "true_t -> ('a, 'a not_t) or_t");
+              TypeOpt("prop_cc4", "true_t -> ('a -> 'c) -> ('a not_t -> 'c) -> 'c");
+              TypeOpt("prop_cc5", "true_t -> ('a, 'a not_t) and_t");
+              TypeOpt("prop_cc5", "true_t -> ('a -> 'a not_t -> 'c) -> 'c");
               TypeOpt("prop_cc6", "(('a -> 'b) -> 'a) -> 'a")];
    hatten 3, [CurryUncurry("h", "f")]]
