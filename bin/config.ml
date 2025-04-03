20bdfbf4f89790d7496ea5cd853b06745461fcdf
sylide <71720482+s-ylide@users.noreply.github.com>
2025-04-04 03:45:08 +0900

    update ocaml version to 5.1.1

--- bin/config.ml
+++ bin/config.ml
@@ -1,6 +1,6 @@
 let version = "v2024.4"
 let no = ref 0
-let ocaml_version = "5.1.0"
+let ocaml_version = "5.1.1"
 let orig_working = Sys.getcwd()
 let dir = "_fl_tmp_" ^ Util.Unix.string_of_time()
 let file_dir = ref ""
