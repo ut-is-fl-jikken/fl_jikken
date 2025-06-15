59d05f0232c54a4d318b07305ae1f08741771607
Ken Sakayori <kensakayori+research@gmail.com>
2025-06-15 18:02:39 +0900

    add alternative options for week 09
    I'm not bumping up the version for such a small commit

--- bin/week09.ml
+++ bin/week09.ml
@@ -4,8 +4,10 @@ open Interpreter
 
 let assignments =
   [report;
-   toi 1 [build];
-   toi 2 [build];
+   toi 1 [build]
+     ~alt:[toi_id 2; toi_id 3];
+   toi 2 [build]
+     ~alt:[toi_id 3];
    toi 3 [build];
    toi 4 [build];
    hatten 1 [build];
