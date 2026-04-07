da8df96660f2ddd84c3210bb5be02e369c12573c
Ken Sakayori <kensakayori+research@gmail.com>
2026-04-07 11:12:05 +0900

    update for week01

--- bin/week01.ml
+++ bin/week01.ml
@@ -9,9 +9,7 @@ let assignments =
           Type("gcd", "int -> int -> int")];
    toi 2 [ValDef "twice";
           ValDef "repeat"];
-   toi 3 [Type("sum_to_fix", "int -> int");
-          Type("is_prime_fix", "int -> bool");
-          Type("gcd_fix", "int -> int -> int")];
+   toi 3 [Type("sum_to_fix", "('a -> 'a -> int) -> 'a list -> 'a list")];
    toi 4 [ValDef "fold_left";
           ValDef "fold_right"];
    toi 5 [Type("append", "'a list -> 'a list -> 'a list");
@@ -21,10 +19,10 @@ let assignments =
           Type("append_right", "'a list -> 'a list -> 'a list");
           Type("filter_right", "('a -> bool) -> 'a list -> 'a list")];
    toi 7 [Type("perm", "'a list -> 'a list list")];
-   hatten 1 [ValDef "reverse";
+   hatten 1 [Type("sum_to_fix", "int -> int");
+          Type("is_prime_fix", "int -> bool");
+          Type("gcd_fix", "int -> int -> int")];
+   hatten 2 [ValDef "reverse";
              ValDef "reverse_right"];
-   hatten 2 [ValDef "fold_left_by_right";
-             ValDef "fold_right_by_left"];
-   hatten 3 [ValDef "add";
-             ValDef "mul";
-             ValDef "sub"]]
+   hatten 3 [ValDef "fold_left_by_right";
+             ValDef "fold_right_by_left"]]
