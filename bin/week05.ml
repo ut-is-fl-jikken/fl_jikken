60078e5b0cef1858f3c75a8482d9b9fd8bb18df3
Ken Sakayori <kensakayori+research@gmail.com>
2026-05-12 08:19:42 +0900

    fix for week 05

--- bin/week05.ml
+++ bin/week05.ml
@@ -1,6 +1,6 @@
 open Assignment_types
 open Edsl
-open Interpreter
+open Ml
 
 let assignments =
   [report;
@@ -11,5 +11,5 @@ let assignments =
    toi 3 [Type("eval", "expr -> value"); ValDef("print_expr"); ValDef("expr_shr"); ValDef("expr_gen")];
    toi 4 [Type("check_inv1", "'a RedBlackTree.t -> bool");
           Type("check_inv2", "'a RedBlackTree.t -> bool")];
-   hatten 1 [];
+   Dir.hatten 1 [];
    hatten 2 [CurryUncurry("h", "f")]]
